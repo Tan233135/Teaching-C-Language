cd4d869a630bc9b51683214f2cf736c8a0370e23
Abdur Rahaman Tanvir <artanvir101@outlook.com>
2026-02-09 23:16:35 +0600

    new

---
+++ CHAPTER 1 (BASICS)/Input,ouput, Data Types/Data Types.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    double a;
+    scanf("%lf",&a); /// '&'?? 
+    printf("%lf",a);
+}
+
+
+///int --> %d  | integer --> whole number
+///float --> %f  | floating point --> fractional number
+///char --> %c   | character --> a,b,c,d. single alphabet
+///double --> %lf   | double sized floating point --> fractional number
+///string --> %s  | string --> collection of characters. While taking string as an input, the '&' is not used.
