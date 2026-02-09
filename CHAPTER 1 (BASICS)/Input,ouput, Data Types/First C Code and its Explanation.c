1748afc888e6211601a960e4feac0780c9214608
Abdur Rahaman Tanvir <artanvir101@outlook.com>
2026-02-09 22:47:06 +0600

    new

---
+++ CHAPTER 1 (BASICS)/Input,ouput, Data Types/First C Code and its Explanation.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/// std --> standard
+/// io  --> input & Output
+/// .h  --> Header file
+
+///int main()
+/// int --> int type function
+/// main() --> function name
+
+int main()
+{                 /// {} --> Curlie Presses
+    printf("Hello, World!"); ///printf --> The 'f' denotes its a function
+    return 0;     /// Every function returns a value to the main function. The main function returns a value of '0' to the OS.
+}
