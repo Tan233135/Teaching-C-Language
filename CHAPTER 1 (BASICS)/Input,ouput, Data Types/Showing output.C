d69c8efb788f2321830a0533155786db4de41dfc
Abdur Rahaman Tanvir <artanvir101@outlook.com>
2026-02-09 22:56:22 +0600

    new

---
+++ CHAPTER 1 (BASICS)/Input,ouput, Data Types/Showing output.C
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    printf("Arduino CC. Made in italy.\n");
+    printf("Twinkle Twinkle, Little start\n");
+    printf("Have i wonder what you are.\n");  ///'\n' it is used to create a new line.
+    
+}
